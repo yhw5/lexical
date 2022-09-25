80adc2119d1e9d9debb87a6b5886819be7a47e7c
Gerard Rovira <gerardrovira@meta.com>
2022-09-25 15:42:52 -0400

    move up/down/delete

--- packages/lexical-present/src/nodes/SlideComponent.tsx
+++ packages/lexical-present/src/nodes/SlideComponent.tsx
@@ -6,8 +6,6 @@
  *
  */
 
-import type {LexicalEditor, NodeKey} from 'lexical';
-
 import './SlideComponent.css';
 
 import {CheckListPlugin} from '@lexical/react/LexicalCheckListPlugin';
@@ -18,7 +16,9 @@ import {ListPlugin} from '@lexical/react/LexicalListPlugin';
 import {MarkdownShortcutPlugin} from '@lexical/react/LexicalMarkdownShortcutPlugin';
 import {LexicalNestedComposer} from '@lexical/react/LexicalNestedComposer';
 import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
+import {$getNodeByKey, $getSelection, LexicalEditor, NodeKey} from 'lexical';
 import * as React from 'react';
+import {useEffect, useState} from 'react';
 
 import CodeHighlightPlugin from '../plugins/CodeHighlightPlugin';
 import CodeSandboxPlugin from '../plugins/CodeSandboxPlugin';
@@ -29,16 +29,90 @@ import TreeViewPlugin from '../plugins/TreeViewPlugin';
 
 export default function SlideComponent({
   nodeKey,
-  slide,
+  slide: _slideName,
   content,
 }: {
   nodeKey: NodeKey;
   slide: string;
   content: LexicalEditor;
 }): JSX.Element {
+  const [isActive, setIsActive] = useState(false);
+  useEffect(() => {
+    setIsActive(content.getEditorState().read(() => $getSelection() !== null));
+    return content.registerUpdateListener(({editorState}) => {
+      setIsActive(editorState.read(() => $getSelection() !== null));
+    });
+  }, [content]);
   return (
-    <div className="SlideComponet__slide-container">
-      <div className="SlideComponet__slide">
+    <div className="SlideComponent__slide-container">
+      <div className="SlideComponent__slide">
+        {isActive && (
+          <div className="SlideComponent__toolbar">
+            <button
+              className="SlideComponent__toolbar-item"
+              onClick={() => {
+                const parentEditor = content._parentEditor;
+                if (parentEditor === null) {
+                  return;
+                }
+                parentEditor.update(() => {
+                  const slide = $getNodeByKey(nodeKey);
+                  if (slide === null) {
+                    return;
+                  }
+                  const previousSlide = slide.getPreviousSibling();
+                  if (previousSlide === null) {
+                    return;
+                  }
+                  previousSlide.insertBefore(slide);
+                });
+              }}
+              title="Move before">
+              <i className="icon slide-up" />
+            </button>
+            <button
+              className="SlideComponent__toolbar-item "
+              onClick={() => {
+                const parentEditor = content._parentEditor;
+                if (parentEditor === null) {
+                  return;
+                }
+                parentEditor.update(() => {
+                  const slide = $getNodeByKey(nodeKey);
+                  if (slide === null) {
+                    return;
+                  }
+                  const nextSlide = slide.getNextSibling();
+                  if (nextSlide === null) {
+                    return;
+                  }
+                  nextSlide.insertAfter(slide);
+                });
+              }}
+              title="Move after">
+              <i className="icon slide-down" />
+            </button>
+            <button
+              className="SlideComponent__toolbar-item"
+              onClick={() => {
+                const parentEditor = content._parentEditor;
+                if (parentEditor === null) {
+                  return;
+                }
+                parentEditor.update(() => {
+                  const slide = $getNodeByKey(nodeKey);
+                  if (slide === null) {
+                    return;
+                  }
+                  alert('Needs fixing');
+                  // slide.remove();
+                });
+              }}
+              title="Delete">
+              <i className="icon slide-delete" />
+            </button>
+          </div>
+        )}
         <LexicalNestedComposer initialEditor={content}>
           <RichTextPlugin
             contentEditable={<ContentEditable />}
