709988e57d5ad3348d4865cf148e0cbb4acab202
Gerard Rovira Sánchez <gerard.rovira.sanchez@gmail.com>
2023-10-22 16:02:19 -0400

    count

--- packages/lexical-react/src/LexicalCharacterLimitPlugin__EXPERIMENTAL.tsx
+++ packages/lexical-react/src/LexicalCharacterLimitPlugin__EXPERIMENTAL.tsx
@@ -6,6 +6,16 @@
  *
  */
 
+import type {LexicalEditor} from 'lexical';
+
+import {$rootTextContent} from '@lexical/text';
+import * as React from 'react';
+import {useEffect, useState} from 'react';
+
+import {useLexicalComposerContext} from './LexicalComposerContext';
+
+type StrlenFn = (text: string) => number;
+
 let textEncoderInstance: null | TextEncoder = null;
 
 function textEncoder(): null | TextEncoder {
@@ -42,13 +52,54 @@ export function CharacterLimitPlugin({
   maxLength,
 }: {
   maxLength: number;
-  strlen?: (text: string) => number;
+  strlen?: StrlenFn;
   children?: (overflowed: number) => JSX.Element;
-}) {
-  const overflow = 0;
+}): null | JSX.Element {
+  useOverlay(maxLength, strlen);
 
   if (children === undefined) {
     return null;
   }
+  return (
+    <OverflowCount strlen={strlen} maxLength={maxLength}>
+      {children}
+    </OverflowCount>
+  );
+}
+
+function useOverlay(maxLength: number, strlen: StrlenFn): void {
+  //
+}
+
+function OverflowCount({
+  strlen,
+  children,
+  maxLength,
+}: {
+  maxLength: number;
+  strlen: StrlenFn;
+  children: (overflowed: number) => JSX.Element;
+}): JSX.Element {
+  const [editor] = useLexicalComposerContext();
+  const [overflow, setOverflow] = useState(() =>
+    overflowCount(editor, maxLength, strlen),
+  );
+  useEffect(() => {
+    return editor.registerTextContentListener((text) => {
+      setOverflow(overflowCount(editor, maxLength, strlen));
+    });
+  }, [editor, maxLength, strlen]);
   return children(overflow);
+}
+
+function overflowCount(
+  editor: LexicalEditor,
+  maxLength: number,
+  strlen: StrlenFn,
+): number {
+  return editor.getEditorState().read(() => $overflowCount(maxLength, strlen));
+}
+
+function $overflowCount(maxLength: number, strlen: StrlenFn): number {
+  return maxLength - strlen($rootTextContent());
 }
